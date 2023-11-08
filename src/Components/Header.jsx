e0fe63e64a83cd55648528fe3aae55ca6edc3e40
Therese <thereseskold@live.com>
2023-11-08 13:08:26 +0100

    made the structure in the header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,3 +1,16 @@
+import { IoCartOutline } from 'react-icons/io5'
+import { GiHamburger }from 'react-icons/gi'
+import './header.css'
+
 const Header = () => {
+	return (
+		<section className='header'>
+		<IoCartOutline className='cart-button' />
+		<img className="logo" src="./src/Assets/Logo.svg" />
+		<GiHamburger className='hamburger-button' />
+		</section>
+	)
 	
 }
+
+export default Header
