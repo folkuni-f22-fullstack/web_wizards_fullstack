1f18d71105036f72889a76bfbebc3130e3051b4e
Therese <thereseskold@live.com>
2023-11-08 15:16:29 +0100

    header done

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,12 +1,27 @@
 import { IoCartOutline } from 'react-icons/io5'
 import { GiHamburger }from 'react-icons/gi'
 import './header.css'
+import { useState } from 'react'
+import logo from '/src/Assets/Logo.svg'
 
 const Header = () => {
+	const [colorChange, setColorChange ] = useState(false)
+
+	function changeOpacityScroll() {
+		if (window.scrollY >= 100) {
+			setColorChange(true)
+			
+		} else {
+			setColorChange(false)
+		}
+	}
+
+	window.addEventListener('scroll', changeOpacityScroll)
+
 	return (
-		<header>
+		<header className={colorChange ? "scroll-opacity" : ''}>
 		<IoCartOutline className='cart-button' />
-		<img className="logo" src="./src/Assets/Logo.svg" />
+		<img className="logo" src={logo} alt='logo'/>
 		<GiHamburger className='hamburger-button' />
 		</header>
 	)
