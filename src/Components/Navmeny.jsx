9c4721a35a5a7f0cbec3e704bbce67bf6b1eba06
AnsoMalm <annsophie.malmqvist85@hotmail.com>
2023-11-08 13:59:01 +0100

    fixed css for my nav-meny

--- src/Components/Navmeny.jsx
+++ src/Components/Navmeny.jsx
@@ -1,27 +1,55 @@
 import './navmeny.css'
-import '../index.css'
-import {NavLink} from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 const Navmeny = () => {
 
 	return (
-		<nav className="nav-meny-container">
+		<>
+		<section className='restaurang-info'>
+			<div className='opening hours-contatiner'>
+				<h1>ÖPPETTIDER</h1>
+				<h1>MÅN-TORS 11-22</h1>
+				<h1>FRE-LÖR 12-02</h1>
+				<h1>SÖN 12-20</h1>
+			</div>
+			<div className='contact-container'>
+				<h3>KONTAKTUPPGIFTER</h3>
+				<h4>MAIL@MAIL.COM</h4>
+				<h4>123-45678901</h4>
+				<h4>ADRESSGATAN 1</h4>
+				<h4>STAD</h4>
+			</div>
+
+		</section>
+		<div className='icon-contatiner'>
 			
+		</div>
+		<nav className="nav-meny-container">
 			<ul className="links-nav-meny">
-				<li className='nav-li'>
-					<NavLink to="/Startpage">HEM</NavLink>
+				<li>
+					<NavLink to="/Startpage">
+						HEM
+					</NavLink>
 				</li>
-				<li className='nav-li'>
-					<NavLink to="/Food-Meny">MENY</NavLink>
+				<li>
+					<NavLink to="/Food-Meny">
+						MENY
+					</NavLink>
 				</li>
-				<li className='nav-li'>
-					<NavLink to="/Om-oss">OM OSS</NavLink>
+				<li>
+					<NavLink to="/Om-oss">
+						OM OSS
+					</NavLink>
 				</li>
-				<li className='nav-li'>
-					<NavLink to="/Kontakt">KONTAKT</NavLink>
+				<li>
+					<NavLink to="/Kontakt">
+						KONTAKT
+					</NavLink>
 				</li>
 			</ul>
 		</nav>
+		</>
+		
 	)
 }
 
