1de3c51cdaf8074789339f12c0d0e859c6a8f7ca
QueAnka <annika.nylin@hotmail.com>
2023-11-09 15:23:13 +0100

    added function to retrieve data

--- src/Routes/FoodMenu.jsx
+++ src/Routes/FoodMenu.jsx
@@ -1,17 +1,32 @@
-import {data} from '../data/testdata'
+import './FoodMenu.css'
+import FoodCard from '../Components/FoodCard'
+import { data } from '../data/testdata'
 
 
 
 const FoodMenu = () => {
 
-	return(
-		<div> 
-			<h1>MENY</h1>
-			<article className="card-container">
-			<img src="" alt="" />
+	const dishes = [...data]
+	console.log(dishes);
 
-			</article>
+	return(
+
+
+		<div className='food-menu'> 
+			<h1>MENY</h1>
+			<ul>
+			{ dishes.map(dish =>(<FoodCard key = {dish.name} dish = {dish} />) ) } 
+			</ul>
+
 			<h1>BARNMENY</h1>
+			{/* <article className="card-container menu-card">
+			<div className='arrow-down'><IoIosArrowDown/></div>
+			<img src="" alt="" />
+			<h3>Bliss orginal</h3>
+			<p className='description'>A timeless favorite with beef patty, lettuce, tomato, onion, and pickles.</p>
+			<p className='price'>6.99</p>
+			<button><BiCartAdd/></button>
+			</article> */}
 			<h1>DRYCKER</h1>
 			<h1>TILLBEHÖR</h1>
 		</div>
--- src/data/testdata.js
+++ src/data/testdata.js
@@ -1,8 +1,7 @@
-export const data ={
-	"menu": {
-	  "Hamburgers": [
+export const data =
+	[
 		{
-		  "name": "Classic Burger",
+		  "name": "Bliss Orginal",
 		  "description": "A timeless favorite with beef patty, lettuce, tomato, onion, and pickles.",
 		  "price": 8.99
 		},
@@ -16,58 +15,5 @@ export const data ={
 		  "description": "Delicious burger with crispy bacon and tangy BBQ sauce.",
 		  "price": 10.99
 		}
-	  ],
-	  "Sides": [
-		{
-		  "name": "French Fries",
-		  "description": "Golden and crispy fries.",
-		  "price": 3.49
-		},
-		{
-		  "name": "Onion Rings",
-		  "description": "Crunchy fried onion rings.",
-		  "price": 4.49
-		},
-		{
-		  "name": "Side Salad",
-		  "description": "Fresh mixed greens with dressing.",
-		  "price": 4.99
-		}
-	  ],
-	  "Drinks": [
-		{
-		  "name": "Soda",
-		  "description": "Choice of cola, lemon-lime, or orange.",
-		  "price": 1.99
-		},
-		{
-		  "name": "Iced Tea",
-		  "description": "Refreshing sweetened or unsweetened tea.",
-		  "price": 2.49
-		},
-		{
-		  "name": "Milkshake",
-		  "description": "Creamy milkshake in various flavors.",
-		  "price": 4.99
-		}
-	  ],
-	  "Dipping Sauces": [
-		{
-		  "name": "Barbecue Sauce",
-		  "description": "Classic BBQ sauce.",
-		  "price": 0.99
-		},
-		{
-		  "name": "Ranch Dressing",
-		  "description": "Creamy ranch for dipping.",
-		  "price": 0.99
-		},
-		{
-		  "name": "Garlic Aioli",
-		  "description": "Savory garlic aioli.",
-		  "price": 1.49
-		}
 	  ]
-	}
-  }
-  
+	
