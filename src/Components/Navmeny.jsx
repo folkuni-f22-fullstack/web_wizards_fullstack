1306c3ad17f7d5157fe4c0cc44d25b5a650e73d5
Forslunddavid <forslund.david@gmail.com>
2023-11-15 16:04:26 +0100

    tack för idag

--- src/Components/Navmeny.jsx
+++ src/Components/Navmeny.jsx
@@ -3,32 +3,59 @@ import { NavLink } from "react-router-dom"
 import miniburger from "../Assets/miniburger.svg"
 import { RxCross1 } from "react-icons/rx"
 import { HashLink } from "react-router-hash-link"
+import { useState } from "react"
 
 // when clicking on the KONTAKT Navlink it should scroll down to the footer on small screens and on desktop it should stay in the navmenu and make the  opening hours smaller and the contact info larger.
-
-function reSizeContacts() {}
 
 const Navmeny = () => {
 	const navMobile = "nav-mobile"
 	const navDesktop = "nav-desktop"
+
+	const [style, setStyle] = useState("large-info")
+
+	const changeStyle = () => {
+		console.log("you just clicked")
+		if (style !== "large-info") setStyle("large-info")
+		else setStyle("small-info")
+	}
+
+	// setStyle((prev) => (prev === "large-info" ? "small-info" : "large-info"))
 
 	return (
 		<>
 			<nav className="nav-container">
 				<section className="opening-hours-contatiner">
 					<ul className="restaurang-info">
-						<li>ÖPPETTIDER</li>
-						<li>MÅN-TORS 11-22</li>
-						<li>FRE-LÖR 12-02</li>
-						<li>SÖN 12-20</li>
+						<li className={style ? "large-info" : "small-info"}>
+							ÖPPETTIDER
+						</li>
+						<li className={style ? "large-info" : "small-info"}>
+							MÅN-TORS 11-22
+						</li>
+						<li className={style ? "large-info" : "small-info"}>
+							FRE-LÖR 12-02
+						</li>
+						<li className={style ? "large-info" : "small-info"}>
+							SÖN 12-20
+						</li>
 					</ul>
 					<div className="contact-container">
 						<ul className="contact-info">
-							<li>KONTAKTUPPGIFTER</li>
-							<li>MAIL@MAIL.COM</li>
-							<li>123-45678901</li>
-							<li>ADRESSGATAN 1</li>
-							<li>STAD</li>
+							<li className={style ? "small-info" : "large-info"}>
+								KONTAKTUPPGIFTER
+							</li>
+							<li className={style ? "small-info" : "large-info"}>
+								MAIL@MAIL.COM
+							</li>
+							<li className={style ? "small-info" : "large-info"}>
+								123-45678901
+							</li>
+							<li className={style ? "small-info" : "large-info"}>
+								ADRESSGATAN 1
+							</li>
+							<li className={style ? "small-info" : "large-info"}>
+								STAD
+							</li>
 						</ul>
 					</div>
 				</section>
@@ -62,7 +89,11 @@ const Navmeny = () => {
 							>
 								KONTAKT
 							</HashLink>
-							<NavLink className={navDesktop} to="/Kontakt">
+							<NavLink
+								onClick={() => changeStyle()}
+								className={navDesktop}
+								to="/"
+							>
 								KONTAKT
 							</NavLink>
 						</li>
