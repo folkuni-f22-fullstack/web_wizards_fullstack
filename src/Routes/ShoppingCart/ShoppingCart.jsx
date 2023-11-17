4bcbf7024f0f55b7f215d8557a894d801040081e
AnsoMalm <annsophie.malmqvist85@hotmail.com>
2023-11-17 09:56:22 +0100

    starting on build shoppingcart

--- src/Routes/ShoppingCart/ShoppingCart.jsx
+++ src/Routes/ShoppingCart/ShoppingCart.jsx
@@ -2,7 +2,16 @@ import "./Shoppingcart.css"
 
 const ShoppingCart = () => {
 	return (
-		<></>
+		<section className="shopping-cart">
+		<h1>Varukorg</h1>
+			<div className="order-card-container">
+				<ul>
+				</ul>	
+			</div>
+			<div className="sum-order-container">
+				<p>Totalt: </p> 
+			</div>
+		</section>
 	)
 }
 
