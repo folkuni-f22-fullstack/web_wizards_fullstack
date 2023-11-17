6ad92aef89c4189e554c30fa3fa55558b3d3da31
AnsoMalm <annsophie.malmqvist85@hotmail.com>
2023-11-17 16:01:48 +0100

    start with shoppingscard for the structure

--- src/Routes/ShoppingCart/ShoppingCart.jsx
+++ src/Routes/ShoppingCart/ShoppingCart.jsx
@@ -1,17 +1,32 @@
 import "./Shoppingcart.css"
+import { useRecoilValue } from "recoil"
+import { cartItemsAtom } from "../../data/atom"
 
 
 const ShoppingCart = () => {
+	// const cart = useRecoilValue(cartItemsAtom)
 	return (
 		<>
 		<section className="shopping-cart">
 		<h1>Varukorg</h1>
-			<div className="order-card-container">
-				<ul>lalala</ul>	
-			</div>
+			{/* <div className="order-card-container"> */}
+				<div className="card-container">
+					<div>bilden</div>
+					<div>Namnet</div>
+					<div>description</div>
+					<div>korg</div>
+					<div className="button-container">
+						<button>-</button>
+						<p>1</p>
+						<button>+</button>
+					</div>
+					<p>Ändra/ta bort i din beställning:</p>
+					<input />
+				</div>
+			{/* </div> */}
 				<div className="sum-order-container">
 					<p>Totalt: </p> 
-					<p>150:-</p>
+					<p></p>
 			</div>
 		</section>
 		</>
