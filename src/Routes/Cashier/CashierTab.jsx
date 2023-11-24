f9dc35c71c60f79ce36564042cf5cc1784ab66f4
Therese <thereseskold@live.com>
2023-11-24 08:48:52 +0100

    started with the tabs

--- src/Routes/Cashier/CashierTab.jsx
+++ src/Routes/Cashier/CashierTab.jsx
@@ -2,15 +2,20 @@ import { NavLink } from "react-router-dom";
 
 import { FaCashRegister } from "react-icons/fa6";
 import './Cashier.css'
+import { useState } from "react";
 
 const CashierTab = () => {
+	const [activeTab, setActiveTab] = useState(false)
 
-	
+	const activeTabClick = () => {
+		setActiveTab(true)
+	}
 
 	return(
-		<div className="cashier-tab-container">
+		<div className= {activeTab ? 'active-tab' : 'cashier-tab-container'}>
 			<NavLink 
-				to='/cashier'>
+				to='/cashier'
+				onClick={activeTabClick}>	
 				<FaCashRegister />
 			</NavLink>
 		</div>
@@ -18,3 +23,4 @@ const CashierTab = () => {
 }
 
 export default CashierTab
+
