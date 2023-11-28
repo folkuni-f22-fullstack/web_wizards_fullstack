eda00ea50a1844b210e725d5cf9e18f35cfeb545
QueAnka <annika.nylin@hotmail.com>
2023-11-28 16:36:25 +0100

    added new put-command

--- webwizardsserverless/backend/LambdaPUTOrders.js
+++ webwizardsserverless/backend/LambdaPUTOrders.js
@@ -1,20 +1,47 @@
-const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
-const { DynamoDBDocument} = require("@aws-sdk/lib-dynamodb")
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import {DynamoDBDocumentClient,PutCommand} from "@aws-sdk/lib-dynamodb";
 
-const client = new DynamoDBClient({})
-const db = DynamoDBDocument.from(client)
+const client = new DynamoDBClient({});
+const dynamo = DynamoDBDocumentClient.from(client);
 
-module.exports.handler = async () => {
+export const handler = async (event) => {
 	const tableName = "orderTable"
+	let requestJSON = JSON.parse(event.body);
 	try {
-		const { Items } = await db.query({
+		const { Items } = await dynamo.send(
+			new PutCommand({
 			TableName: tableName,
+			Item:{
+				"items": [
+		{
+			"orderContent": [
+				{
+					"name": requestJSON.name,
+					"description": requestJSON.description,
+					"amount": Number.requestJSON.amount,
+					"id": requestJSON.id,
+					"message": requestJSON.message,
+					"price": Number.requestJSON.price
+				},
+				{
+					"familyname": requestJSON.familyname,
+					"firstname": requestJSON.firstname,
+					"id": requestJSON.id,
+					"phone": requestJSON.phone,
+					"email": requestJSON.email
+				}
+			],
+			"pk": "orders",
+			"ordersId": requestJSON.ordersId
+			}
+		]}
 			// IndexName: "sk",
-			KeyConditionExpression: "pk = :pk",
-			ExpressionAttributeValues: {
-				":pk": "orders",
-			},
+			// KeyConditionExpression: "pk = :pk",
+			// ExpressionAttributeValues: {
+			// 	":pk": "orders",
+			// },
 		})
+		)
 
 		return {
 			statusCode: 200,
