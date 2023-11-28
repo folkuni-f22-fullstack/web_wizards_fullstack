534b848f7b615613fd51bfda3b82b80a0ca1b87a
QueAnka <annika.nylin@hotmail.com>
2023-11-28 13:06:34 +0100

    delete lamda-function working!

--- webwizardsserverless/backend/LambdaDELETEOrders.js
+++ webwizardsserverless/backend/LambdaDELETEOrders.js
@@ -1,35 +1,66 @@
 const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
-const { DynamoDBDocument} = require("@aws-sdk/lib-dynamodb")
+const { DynamoDBDocumentClient, DeleteCommand, GetCommand } = require("@aws-sdk/lib-dynamodb");
 
-const client = new DynamoDBClient({})
-const db = DynamoDBDocument.from(client)
+const client = new DynamoDBClient({});
+const dynamo = DynamoDBDocumentClient.from(client);
 
-module.exports.handler = async (event) => {
-	const tableName = "orderTable"
-	const primaryKey = 'pk';
-	const ordersId = event.pathParameters.ordersId;
-	console.log('delete order: ', event)
-	try {
-		await db.delete({
-			TableName: tableName,
-			// IndexName: "sk",
-			Key: {
-				pk: primaryKey,
-				ordersId: ordersId
+const tableName = "orderTable";
 
-			},
-		});
+exports.handler = async (event) => {
+  let body;
+  let statusCode = 200;
+  const headers = {
+    "Content-Type": "application/json",
+  };
 
-		return {
-			statusCode: 200,
-			headers: { "Content-Type": "application/json" },
-			body: JSON.stringify({ message: "success, item deleted" }),
-		}
-	} catch (err) {
-		return {
-			statusCode: err.statusCode,
-			headers: { "Content-Type": "application/json" },
-			body: JSON.stringify({ message: err.message }),
-		}
-	}
-}
+  try {
+    const ordersId = event.pathParameters.ordersId.toString();
+
+    // Check if the item exists before attempting to delete
+    const getItemResponse = await dynamo.send(
+      new GetCommand({
+        TableName: tableName,
+        Key: {
+          "pk": "orders", // Assuming "pk" is the primary key value you are using
+          "ordersId": ordersId,
+        },
+      })
+    );
+
+    console.log("getItemResponse:", getItemResponse);
+
+    if (!getItemResponse.Item) {
+      // Item not found
+      statusCode = 404;
+      body = JSON.stringify({ message: "Item not found" });
+    } else {
+      console.log("Item exists, proceeding with deletion");
+
+      // Item exists, proceed with deletion
+      await dynamo.send(
+        new DeleteCommand({
+          TableName: tableName,
+          Key: {
+            "pk": "orders",
+            "ordersId": ordersId,
+          },
+        })
+      );
+
+      body = `Deleted order with id: ${ordersId}`;
+    }
+  } catch (error) {
+    statusCode = 500;
+    body = error.message;
+    console.error("Error:", error);
+  } finally {
+    console.log("Response:", { statusCode, body, headers });
+    body = JSON.stringify(body);
+  }
+
+  return {
+    statusCode,
+    body,
+    headers,
+  };
+};
