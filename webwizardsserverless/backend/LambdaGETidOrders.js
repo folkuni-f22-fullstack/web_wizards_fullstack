2d5bc3e2543ce742987f44bd88654ebd81a4972f
Therese <thereseskold@live.com>
2023-11-29 09:48:59 +0100

    all lambda functions done

--- webwizardsserverless/backend/LambdaGETidOrders.js
+++ webwizardsserverless/backend/LambdaGETidOrders.js
@@ -1,35 +1,53 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocumentClient, QueryCommand } from "@aws-sdk/lib-dynamodb";
+import { DynamoDBDocumentClient, GetCommand } from "@aws-sdk/lib-dynamodb";
 
 const client = new DynamoDBClient({});
 const dynamo = DynamoDBDocumentClient.from(client);
 
-export const handler = async () => {
-    const tableName = "orderTable";
-    const partitionKey = "orders"; // pk value
-    // const sortKey = dynamically provide or calculate the value based on your logic
+const tableName = "orderTable";
 
-    try {
-        const { Items } = await dynamo.send(
-            new QueryCommand({
-                TableName: tableName,
-                KeyConditionExpression: "pk = :pk",
-                ExpressionAttributeValues: {
-                    ":pk": partitionKey,
-                },
-            })
-        );
+export const handler = async (event) => {
+  let body;
+  let statusCode = 200;
+  const headers = {
+    "Content-Type": "application/json",
+  };
 
-		return {
-			statusCode: 200,
-			headers: { "Content-Type": "application/json" },
-			body: JSON.stringify({ items: Items }),
-		}
-	} catch (err) {
-		return {
-			statusCode: err.statusCode,
-			headers: { "Content-Type": "application/json" },
-			body: JSON.stringify({ message: err.message }),
-		}
-	}
-}
+  try {
+    const ordersId = event.pathParameters.ordersId.toString();
+
+    // Retrieve the item based on ordersId
+    const getItemResponse = await dynamo.send(
+      new GetCommand({
+        TableName: tableName,
+        Key: {
+          "pk": "orders",
+          "ordersId": ordersId,
+        },
+      })
+    );
+
+    console.log("getItemResponse:", getItemResponse);
+
+    if (!getItemResponse.Item) {
+      // Item not found
+      statusCode = 404;
+      body = JSON.stringify({ message: `Order with id ${ordersId} not found` });
+    } else {
+      // Item found, return it
+      body = JSON.stringify({ order: getItemResponse.Item });
+    }
+  } catch (error) {
+    statusCode = 500;
+    body = JSON.stringify({ message: error.message || "Internal Server Error" });
+    console.error("Error:", error);
+  } finally {
+    console.log("Response:", { statusCode, body, headers });
+  }
+
+  return {
+    statusCode,
+    body,
+    headers,
+  };
+};
