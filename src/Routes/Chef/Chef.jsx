8605e72c9ed3bbd024795af474576538f2ee8d05
Forslunddavid <forslund.david@gmail.com>
2023-11-30 16:53:24 +0100

    imports, testdata and function added

--- src/Routes/Chef/Chef.jsx
+++ src/Routes/Chef/Chef.jsx
@@ -2,8 +2,77 @@ import "./Chef.css"
 import KeepLoggedIn from "../../utils/login/KeepLoggedIn"
 import getOrders from "../../utils/APIfrontendFunctions/getOrders"
 import { FiRefreshCcw } from "react-icons/fi"
+import { useState, useEffect } from "react"
 
 const Chef = () => {
+	//Get enbart om orderOpen === false
+
+	// const [data, setData] = useState([])
+
+	// useEffect(() => {
+	// 	const fetchData = async () => {
+	// 		const menuData = await getMenu()
+	// 		setData(menuData.items)
+	// 	}
+	// 	fetchData()
+	// }, [])
+
+	// testvariabel:
+	const orders = [
+		{
+			ordersId: "1234",
+			orderOpen: false,
+			orderContent: [
+				{
+					name: "Bliss",
+					description: "tomat",
+					price: 79,
+					amount: 1,
+					message: "ingen tomat",
+				},
+				{
+					name: "Halloumi",
+					description: "gurka",
+					price: 79,
+					amount: 2,
+					message: "ingen gurka",
+				},
+			],
+			costumerInfo: {
+				firstname: "my",
+				familyname: "Myson",
+				phone: 123546,
+				email: "abc@abs",
+			},
+		},
+		{
+			ordersId: "5678",
+			orderOpen: true,
+			orderContent: [
+				{
+					name: "Blobb",
+					description: "tomat",
+					price: 79,
+					amount: 1,
+					message: "ingen tomat",
+				},
+				{
+					name: "Orginal",
+					description: "gurka",
+					price: 79,
+					amount: 2,
+					message: "ingen gurka",
+				},
+			],
+			costumerInfo: {
+				firstname: "my",
+				familyname: "Myson",
+				phone: 123546,
+				email: "abc@abs",
+			},
+		},
+	]
+
 	return (
 		<section className="chef_container">
 			<KeepLoggedIn />
