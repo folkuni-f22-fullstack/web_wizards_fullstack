7a9dc4427ed68ea91d54607110b516343a73d55f
AnsoMalm <annsophie.malmqvist85@hotmail.com>
2023-12-02 14:48:15 +0100

    fixed putOrder.js and fixed to get orders into confirmation.

--- src/Routes/Confirmation/Confirmation.jsx
+++ src/Routes/Confirmation/Confirmation.jsx
@@ -1,19 +1,24 @@
 import "./confirmation.css"
 import { FiRefreshCcw } from "react-icons/fi" 
 import { useEffect, useState } from "react"
-import { useRecoilValue } from 'recoil'
+import { useRecoilValue, useRecoilState } from 'recoil'
 import { cartItemState } from "../../data/atom"
 import getOrdersId from "../../utils/APIfrontendFunctions/GetOrdersId"
+import { IoRemoveOutline, IoTrashSharp } from "react-icons/io5"
+import { IoMdAdd } from "react-icons/io"
 import useRemoveFromCart from "../../utils/removeFromCart"
-// import '../ShoppingCart/ShoppingCart.css'
+// import { putOrder } from "../../utils/APIfrontendFunctions/PostOrder"
+import { costumerAtom } from "../../data/atom"
 
 // Behöver importera datan från varukorgen så att vi kan mappa ut den.
 
 // eventuella states som kan behövas för att rendera om beställningen är öppen eller låst: 
 
 const Confirmation = () => {
+	const userInput = useRecoilValue(costumerAtom) /* <--Detta har jag lagt till ifall jag ska ha?*/
 	const cartItems = useRecoilValue(cartItemState)
-	// const removeFromCart = useRemoveFromCart()
+	const removeFromCart = useRemoveFromCart()
+	const [, setCartItems] = useRecoilState(cartItemState)
 
 	const [data, setData] = useState({})
 
@@ -28,45 +33,72 @@ const Confirmation = () => {
 
 	console.log('useState data : ', data);
 
-	const order = data.order.orderContent
+	const orderContent = data.order && data.order.orderContent
+	console.log('Min ordercontent: ', orderContent)
 
-	console.log('order', order);
+	const orderId = data.order && data.order.ordersId
+
+	console.log('kundens orderid: ', orderId)
+
+	// const order = data.order.orderContent
+	// console.log('order', order);
 
 
 	// const handleClickUpdate = () => {
 	// 	setOpenOrder(!openOrder)
 	// }
-	// const handleRemoveFromCart = (name) => {
-	// 	removeFromCart(name)
-	// 	console.log('removed')
-	// }
-	const countPriceTotal = cartItems.reduce((total, cartItem) => total + cartItem.priceTotal, 0)
+	const handleRemoveFromCart = (name) => {
+		removeFromCart(name)
+		console.log('removed')
+	}
+	// const countPriceTotal = cartItems.reduce((total, cartItem) => total + cartItem.priceTotal, 0)
+
+	const handleOrderChangeSubmit = async (event) => {
+		try {
+			event.preventDefualt 
+
+			await setCartItems((prevCartItems) => [...prevCartItems])
+
+			await putOrder(cartItems, userInput, ordersId)
+			console.log("sucess, den ändrade ordern är skickad till restaurangen.")
+			cartItems, 
+			userInput
+		} catch (error) {
+			console.log("error, orderns ändringar är inte skickade.", error.message)
+		}
+	}
+
+	const handleInputMessage = (event, item) => {
+		const orderItems = orderContent.cartItems.map((cartItem) => cartItem.name === item?.name ? {...cartItem, message: event.target.value} : cartItem
+		)
+		setCartItems(orderItems)
+	}
 
 	return (
 		<section className="confirmation_container">
 			<h1 className="head_confirmation">Bekräftelse</h1>
 			<div className="update-container">
-				{/* <button onClick={handleClickUpdate} className="update-button">
+				<button className="update-button">
                         <FiRefreshCcw />
-                </button> */}
+                </button>
 			</div>
 			<div className="order_confirmation_info">
-					<h2>Ordernummer: </h2>
+					<h2>Ordernummer: {data.order && data.order.ordersId} </h2>
 				 <div className="open_order_text">
 					<h3>Nu är din order skickad till restaurangen.</h3>
 					
 					<p> Vill du ändra något i din beställning? </p>
 					<p>Passa på nu innan beställningen blir låst.</p>
-				</div> :
-				<div className="locked_order_text">
+				</div> 
+				{/* <div className="locked_order_text">
 					<h3>Nu är din beställning låst och maten tillagas</h3>
-				</div>
+				</div> */}
 			</div>
 			
 				<h3 className="head_your_order">Din beställning: </h3>
 
 			<section className="shopping-cart">
-			{order.cartItems.map(item => (
+			{orderContent && orderContent.cartItems.map(item => (
 				<li key={item.name} className="card-container order-menu">
 					
 					<div className="image-container">
@@ -76,26 +108,37 @@ const Confirmation = () => {
 						<h3>{item.name}</h3>
 					</div>
 					<p className="description-text">{item.description}</p>
-					{/* <div className="button-container">
+					<div className="button-container">
 						<IoRemoveOutline className="remove-food"/>
 						<p>{item.amount}</p>
 						<IoMdAdd className="add-food" />
-					</div> */}
+					</div>
 					<p className="food-price">{item.priceTotal} :-</p>
-					{/* <div onClick={() => handleRemoveFromCart(item.name)} className="dumpster">
+					<div onClick={() => handleRemoveFromCart(item.name)} className="dumpster">
 						<IoTrashSharp  className="trashbin"/>
-					</div> */}
+					</div>
 					<div className="input">
 						<p>Ändra/ta bort i din rätt:</p>
-						<input className="input change"/>
+						<input 
+							onChange={(event) => 
+								handleInputMessage(event, item)}	
+							className="input change"
+						/>
 					</div>
 					
 				</li>))}
-				<div className="sum-order-container">
+				{/* <div className="sum-order-container">
 					<p>Totalt:</p> 
 					<p>{countPriceTotal} :- </p>
-				</div>
+				</div> */}
 			</section>
+			<button
+				type="submit"
+				className="change-order-button"
+				onClick={() => handleOrderChangeSubmit(cartItems)}
+			>
+				Ändra order
+			</button>
 
 		</section>
 	)
---
+++ src/utils/APIfrontendFunctions/PutOrder.js
@@ -0,0 +1,35 @@
+
+
+export async function putOrder(cartItems, orderId) {
+	try {
+		const response = await fetch(
+		'https://omw2d3h4o6.execute-api.eu-north-1.amazonaws.com/webwizards/orders/', {
+			method: 'PUT',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				"items": [ {
+					"orderContent": {
+						cartItems,
+						"costumerInfo": userInput,
+						"orderLocked": false,
+						// "orderId": orderId
+					} 
+				}
+				]
+			}),
+			
+		});
+		
+		if (!response.ok){
+			throw new Error('Failed to post new order');
+		}
+			const data = await response.json();
+			console.log(data);
+			return data 
+		
+} catch(error) {
+		console.error('Error posting data:', error.message);
+	}
+}
